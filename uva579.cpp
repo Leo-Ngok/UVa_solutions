858c74fc8fbabfa61da6550f064e50b4af434e0a
Leo-Ngok <Leo-Ngok-official@users.noreply.github.com>
2019-08-15 19:19:49 +0800

    Create uva579.cpp

---
+++ uva579.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    double hr,mn;
+    char dot;
+    while(true){
+        cin>>hr>>dot>>mn;
+        if(hr==0&&mn==0) break;
+        hr=-hr*30-mn/2;
+        mn*=-6;
+        double res=abs(hr-mn);
+        if(res>180) res=360-res;
+        printf("%.3f\n",res);
+    }
+}
