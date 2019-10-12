bef0aa85b599c835b56b5a36557da5be7790d5a8
Ngok Leo <z1023825@outlook.com>
2019-10-12 19:04:54 +0800

    Update p1598_0.cpp

--- p1598_0.cpp
+++ p1598_0.cpp
@@ -7,26 +7,18 @@ int main(){
 	char words[WORDLEN]; 
 	for(int g=0;g<4;g++){		
 		gets(words);
-		for(int k=0;k<WORDLEN;k++){
+		for(int k=0;k<WORDLEN;k++){		
 			if(words[k]=='\0') break;
 			if(words[k]>='A'&&words[k]<='Z')anum[words[k]-'A']++;		
 		}
 	}
-	int max;
+	int max=0;
 	for(int q=0;q<26;q++)if(max<anum[q])max=anum[q];	
 	for(int j=max;j>=1;j--){
 		char starlist[26];
-		for(int r=0;r<26;r++)
-			if(anum[r]>=j) printf("* ");//starlist[r]='*';
-			else printf("  ");//starlist[r]=' ';
-		
-		/*for(int u=26;u>=0;u--){
-			if(starlist[u]!='*') starlist[u]='\0';
-			else break;
-		}
-		printf("%s\n",starlist);*/
+		for(int r=0;r<26;r++) if(anum[r]>=j) printf("* "); else printf("  ");
 		printf("\n");
 	}
-	for(char a='A';a<='Z';a++)printf("%c ",a);
+	for(int a=0;a<=25;a++)printf("%c ",a+'A');
 	return 0;
 }
